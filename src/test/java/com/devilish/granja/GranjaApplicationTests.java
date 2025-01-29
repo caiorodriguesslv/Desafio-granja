a8f18fed9652abbf39d853d91785f193f618dbc9
unknown <caiorodriguesmll@gmail.com>
2025-01-29 16:50:19 -0300

    first commit

---
+++ src/test/java/com/devilish/granja/GranjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devilish.granja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
